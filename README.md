9b3db39901b74e0b8a7448350fb7b3c7bf9f275e
unknown <jideshvaidya@gmail.com>
2020-01-12 14:50:55 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"this is curd app taking refrence from codeaffection technology used are asp.net core and angular 8" 
---
+++ WebApi/ClientApp/src/app/shared/bank.service.ts
@@ -0,0 +1,13 @@
+import { environment } from "./../../environments/environment";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root"
+})
+export class BankService {
+  constructor(private http: HttpClient) {}
+  getBankList() {
+    return this.http.get(environment.apiBaseURI + "/bank");
+  }
+}
